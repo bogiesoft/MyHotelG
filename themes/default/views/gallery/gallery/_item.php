b693fc129bded3998cc57fa50627cbba37398561
Pavel Prohorov <redperchic@mail.ru>
2016-07-13 19:56:40 +0700

    change gallery

--- themes/default/views/gallery/gallery/_item.php
+++ themes/default/views/gallery/gallery/_item.php
@@ -12,15 +12,17 @@
  * @var $data Gallery
  **/
 ?>
-<div class="grid_4">
+<div class="grid_12">
     <div class="media">
-        <h2 class="media-heading" style="font-size: 20px;text-align: left">
-            <?= CHtml::link(CHtml::encode($data->name), ['/gallery/gallery/view', 'id' => $data->id]); ?>
-        </h2>
-        <?= CHtml::link(
-            CHtml::image($data->previewImage(600, 600), $data->name, ['class' => 'thumbnail media-object']),
-            ['/gallery/gallery/view', 'id' => $data->id],
-            ['class' => 'pull-left']
-        ); ?>
+        <div class="gallery-show">
+            <h2 class="media-heading" style="font-size: 20px;text-align: left">
+                <?= CHtml::encode($data->name); ?>
+            </h2>
+            <?php $this->widget(
+                'gallery.widgets.GalleryWidget',
+                ['galleryId' => $data->id, 'gallery' => $data, 'limit' => 30]
+            ); ?>
+        </div>
+
     </div>
 </div>

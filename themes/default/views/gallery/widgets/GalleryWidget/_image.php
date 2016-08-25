0ceb14c10387c3077a7f9c2bd00f72f0f3c4b771
u4aew <dialoggg1994@gmail.com>
2016-08-25 12:25:07 +0700

    fixed

--- themes/default/views/gallery/widgets/GalleryWidget/_image.php
+++ themes/default/views/gallery/widgets/GalleryWidget/_image.php
@@ -1,6 +1,6 @@
 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6" style="padding: 10px">
         <div class="b-gallery__mini-pic" style="background-image: url('<?= $data->image->getImageURL(); ?>')">
-            <a rel="group" class="b-gallery__link fancybox" href="<?= $data->image->getImageURL(); ?>"></a>
+            <a rel="group" class="b-gallery__link fancybox" title="<?= $data->image->description; ?>" href="<?= $data->image->getImageURL(); ?>"></a>
         </div>
 </div>
 
